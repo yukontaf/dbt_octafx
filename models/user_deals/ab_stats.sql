0cd63c6db906dd0157e8bc03e9511753480a3d23
Gleb Sokolov <yukontaf@gmail.com>
2024-03-27 03:30:33 +0000

    added percentile calculation to ab_stats model

--- models/user_deals/ab_stats.sql
+++ models/user_deals/ab_stats.sql
@@ -1,39 +1,76 @@
-SELECT
-    ab.user_id,
-    ab.variant,
-    COUNT(DISTINCT d.operation_id) AS deals_cnt,
-    SUM(d.volume) AS symbol_volume,
-    SUM(
-        CASE
-            WHEN d.symbol_name = '{{ var('symbol_name') }}' THEN d.volume ELSE 0
-        END
-    )
-        AS {{ var('symbol_name') }}_vol,
-    MIN(
-        CASE
-            WHEN d.symbol_name = '{{ var('symbol_name') }}' THEN d.open_time_dt
-        END
-    )
-        AS first_{{ var('symbol_name') }}_deal,
-    SUM(CASE WHEN d.symbol_name = '{{ var('symbol_name') }}' THEN 1 ELSE 0 END)
-        AS {{ var('symbol_name') }}_deals_cnt,
-    IF(
+with stats as (
+    select
+        ab.user_id,
+        ab.variant,
+        COUNT(distinct d.operation_id) as deals_cnt,
+        SUM(d.volume) as symbol_volume,
         SUM(
-            CASE WHEN d.symbol_name = '{{ var('symbol_name') }}' THEN 1 ELSE 0 END
+            case
+                when
+                    d.symbol_name = '{{ var('symbol_name') }}'
+                    then d.volume else
+                    0
+            end
         )
-        > 0,
-        1,
-        0
-    )
-        AS {{ var('symbol_name') }}_converted
-FROM
-    {{ ref('ab_users') }} AS ab
-LEFT JOIN {{ ref('user_deals') }} AS d
-    ON
-        ab.user_id = d.user_id
-        AND DATE(
-            d.close_time_dt
-        ) BETWEEN '{{ var('start') }}' AND '{{ var('end') }}'
-        AND d.variant IS NOT NULL
-GROUP BY
-    ab.user_id, ab.variant
+            as {{ var('symbol_name') }}_vol,
+        MIN(
+            case
+                when
+                    d.symbol_name = '{{ var('symbol_name') }}'
+                    then d.open_time_dt
+            end
+        )
+            as first_{{ var('symbol_name') }}_deal,
+        SUM(
+            case
+                when d.symbol_name = '{{ var('symbol_name') }}' then 1 else 0
+            end
+        )
+            as {{ var('symbol_name') }}_deals_cnt,
+        IF(
+            SUM(
+                case
+                    when d.symbol_name = '{{ var('symbol_name') }}' then 1 else
+                        0
+                end
+            )
+            > 0,
+            1,
+            0
+        )
+            as {{ var('symbol_name') }}_converted
+    from
+        {{ ref('ab_users') }} as ab
+    left join {{ ref('user_deals') }} as d
+        on
+            ab.user_id = d.user_id
+            and DATE(
+                d.close_time_dt
+            ) between '{{ var('start') }}' and '{{ var('end') }}'
+            and d.variant is not NULL
+    group by
+        ab.user_id, ab.variant
+)
+
+select
+    user_id,
+    variant,
+    deals_cnt,
+    PERCENT_RANK()
+        over (partition by user_id order by deals_cnt)
+        as deals_cnt_percentile,
+    symbol_volume,
+    PERCENT_RANK()
+        over (partition by user_id order by symbol_volume)
+        as symbol_volume_percentile,
+    {{ var('symbol_name') }}_vol,
+    PERCENT_RANK()
+        over (partition by user_id order by {{ var('symbol_name') }}_vol)
+        as {{ var('symbol_name') }}_vol_percentile,
+    first_{{ var('symbol_name') }}_deal,
+    {{ var('symbol_name') }}_deals_cnt,
+    PERCENT_RANK()
+        over (partition by user_id order by {{ var('symbol_name') }}_deals_cnt)
+        as {{ var('symbol_name') }}_deals_cnt_percentile,
+    {{ var('symbol_name') }}_converted
+from stats
