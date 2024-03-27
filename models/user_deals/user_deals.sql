56984996cd89de6c04ec0e9dddd7ac9983697f47
Gleb Sokolov <yukontaf@gmail.com>
2024-03-27 07:12:33 +0000

    user_deals modified

--- models/user_deals/user_deals.sql
+++ models/user_deals/user_deals.sql
@@ -1,21 +1,19 @@
-SELECT
+SELECT 
     u.user_id,
     u.variant,
-    operation_id,
-    symbol_name,
-    cmd,
-    volume,
-    open_price,
-    close_price,
-    profit,
-    open_time_dt,
-    close_time_dt,
-    DATE(DATE_TRUNC(open_time_dt, DAY)) AS trade_day
-FROM
+    COALESCE(t.operation_id, 0) AS operation_id,
+    COALESCE(t.symbol_name, '') AS symbol_name,
+    COALESCE(t.cmd, 0) AS cmd,
+    COALESCE(t.volume, 0) AS volume,
+    COALESCE(t.open_price, 0) AS open_price,
+    COALESCE(t.close_price, 0) AS close_price,
+    COALESCE(t.profit, 0) AS profit,
+    COALESCE(t.open_time_dt, CAST('1970-01-01' AS TIMESTAMP)) AS open_time_dt,
+    COALESCE(t.close_time_dt, CAST('1970-01-01' AS TIMESTAMP)) AS close_time_dt,
+    COALESCE(DATE(DATE_TRUNC(t.open_time_dt, DAY)), DATE('1970-01-01')) AS trade_day
+FROM 
     {{ ref('ab_users') }} u
-LEFT JOIN  {{ source("wh_raw", "trading_real_raw") }} AS t
-    ON u.user_id = t.user_id
-WHERE
-    (DATE(open_time_dt) BETWEEN '{{ var('start') }}' AND '{{ var('end') }}')
-    AND (DATE(close_time_dt) BETWEEN '{{ var('start') }}' AND '{{ var('end') }}')
-    AND cmd < 2
+    LEFT JOIN {{ source("wh_raw", "trading_real_raw") }} AS t ON u.user_id = t.user_id
+        AND (DATE(t.open_time_dt) BETWEEN '{{ var('start') }}' AND '{{ var('end') }}')
+        AND (DATE(t.close_time_dt) BETWEEN '{{ var('start') }}' AND '{{ var('end') }}')
+        AND t.cmd < 2
