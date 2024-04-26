8878af92a0a1bf2fda836f7f3e3102a07541a8b5
Gleb Sokolov <yukontaf@gmail.com>
2024-04-26 13:59:47 +0300

    changes
    # Conflicts:
    #	dependencies.yml
    #	models/push_problem/dummy_id.yml
    #	models/push_problem/int_pushes.sql
    #	package-lock.yml
    gg
    
    Signed-off-by: Gleb Sokolov <yukontaf@gmail.com>

--- models/push_problem/int_pushes.sql
+++ models/push_problem/int_pushes.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
    SELECT 
       *,
       CASE 
@@ -20,3 +21,59 @@
       AND date(timestamp) BETWEEN '2024-01-01' AND date(current_timestamp)
       AND status IN ('delivered', 'failed')
     )
+=======
+{{
+    config(
+        materialized="view",
+        partition_by={
+            "field": "timestamp",
+            "data_type": "timestamp",
+            "granularity": "day",
+        },
+        clustering=["user_id"],
+    )
+}}
+
+with
+    users as (
+
+        select user_id, registered_dt, country
+        from {{ source('wh_raw', 'users') }}
+        where registered_dt >= '2024-01-01'
+
+    ),
+
+    countries as (select * from {{ source('wh_raw', 'countries') }}),
+    
+    tokens as (
+        select internal_id, raw_properties.google_push_notification_id as token
+        from {{source('bloomreach', 'customers_properties')}}
+    ),
+
+    users_countries as (
+
+        select user_id, registered_dt, code
+        from users u
+        left join countries c on u.country = c.country
+    ),
+
+    filtered_events as (
+        select *
+        from {{ref('int_bloomreach_events_enhanced')}}
+        where action_type = 'mobile notification' and status = 'delivered'
+    ),
+
+    installed as (
+        select customer_user_id, event_time_dt from 
+        {{source('wh_raw', 'mobile_appsflyer')}} a
+        inner join users_countries u on a.customer_user_id = u.user_id 
+        where event_time_dt >= '2024-01-01'
+        AND event_type = 'install'
+    )
+
+SELECT
+    uc.user_id,
+    t.token
+FROM installed
+
+>>>>>>> 3aefc99 (changes)
