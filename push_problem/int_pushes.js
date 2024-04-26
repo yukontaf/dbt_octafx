13f39345a37b3062ccfec64686338334d70b5421
Gleb Sokolov <yukontaf@gmail.com>
2024-04-26 16:44:56 +0300

    cosmetic changes

--- push_problem/int_pushes.js
+++ push_problem/int_pushes.js
@@ -1,21 +1,21 @@
 cube(`int_pushes`, {
   sql: `
-    SELECT 
+    SELECT
       *,
-      CASE 
-        WHEN event_order = 1 THEN timestamp 
-        ELSE NULL 
-      END AS first_event 
+      CASE
+        WHEN event_order = 1 THEN timestamp
+        ELSE NULL
+      END AS first_event
     FROM (
-      SELECT 
+      SELECT
         *,
         ROW_NUMBER() OVER (
           PARTITION BY user_id
           ORDER BY timestamp
-        ) AS event_order 
+        ) AS event_order
       FROM dev_gsokolov.int_bloomreach_events_enhanced
       WHERE safe_cast(user_id as int64) IN (
-        SELECT user_id 
+        SELECT user_id
         FROM ${eligible_users.sql()}
       )
       AND action_type = 'mobile notification'
@@ -27,12 +27,12 @@ cube(`int_pushes`, {
   measures: {
     count: {
       type: `count`,
-      drillMembers: [userId]
+      drillMembers: [userId],
     },
     count_distinct: {
       type: `count_distinct`,
-      sql: `user_id`
-    }
+      sql: `user_id`,
+    },
   },
 
   dimensions: {
@@ -40,23 +40,27 @@ cube(`int_pushes`, {
       sql: `user_id`,
       type: `number`,
       primaryKey: true,
-      public: true
+      public: true,
     },
     eventOrder: {
       sql: `event_order`,
-      type: `number`
+      type: `number`,
+    },
+    status: {
+      sql: `status`,
+      type: `string`,
     },
     status_num: {
       sql: `CASE WHEN ${CUBE}.status='delivered' THEN 1 ELSE 0 END`,
-      type: `number`
+      type: `number`,
     },
     action_type: {
       sql: `action_type`,
-      type: `string`
+      type: `string`,
     },
     timestamp: {
       sql: `timestamp`,
-      type: `time`
-    }
-  }
+      type: `time`,
+    },
+  },
 });
