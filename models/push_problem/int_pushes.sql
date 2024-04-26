a21496cd47f01be17dc3ffdf52dbc9b8803a23a9
Gleb Sokolov <yukontaf@gmail.com>
2024-04-26 11:19:08 +0300

    changes

--- models/push_problem/int_pushes.sql
+++ models/push_problem/int_pushes.sql
@@ -1,22 +1,12 @@
-   SELECT 
-      *,
-      CASE 
-        WHEN event_order = 1 THEN timestamp 
-        ELSE NULL 
-      END AS first_event 
-    FROM (
-      SELECT 
-        *,
-        ROW_NUMBER() OVER (
-          PARTITION BY user_id
-          ORDER BY timestamp
-        ) AS event_order 
-      FROM {{ref('int_bloomreach_events_enhanced')}}
-      WHERE safe_cast(user_id as int64) IN (
-        SELECT user_id 
-        FROM {{ref('eligible_users')}}
-      )
-      AND action_type = 'mobile notification'
-      AND date(timestamp) BETWEEN '2024-01-01' AND date(current_timestamp)
-      AND status IN ('delivered', 'failed')
-    )
+with subq as (SELECT
+*
+FROM {{ref('int_bloomreach_events_enhanced')}}
+WHERE safe_cast(user_id AS int64) IN (
+SELECT user_id
+FROM {{ref('eligible_users')}}
+)
+AND action_type = 'mobile notification'
+AND status IN ('delivered', 'failed')
+)
+select user_id, date(timestamp) as timestamp, status, action_type, error
+from subq
