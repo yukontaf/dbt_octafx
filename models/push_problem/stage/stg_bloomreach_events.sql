9c5c8ae4e56f42866a89bb264f0d3b99918df984
Gleb Sokolov <yukontaf@gmail.com>
2024-05-03 22:38:45 +0300

    minor changes

--- models/push_problem/stage/stg_bloomreach_events.sql
+++ models/push_problem/stage/stg_bloomreach_events.sql
@@ -8,20 +8,20 @@
         clustering=["user_id"]
     ) }}
 
-    SELECT
-        internal_customer_id
-        , SAFE_CAST(user_id AS INT64) as user_id
-        , timestamp
-        , campaign_id
-        , action_id
-        , type
-        , properties.campaign_name
-        , properties.status
-        , properties.error
-        , properties.action_name
-        , properties.action_type
-        , properties.variant
-        , properties.platform
-        , ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY timestamp ASC) AS event_number
-    FROM bloomreach_raw.campaign
-    WHERE timestamp >= '2024-01-01'
+SELECT
+    internal_customer_id
+    , SAFE_CAST(user_id AS INT64) as user_id
+    , timestamp
+    , campaign_id
+    , action_id
+    , type
+    , properties.campaign_name
+    , properties.status
+    , properties.error
+    , properties.action_name
+    , properties.action_type
+    , properties.variant
+    , properties.platform
+    , ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY timestamp ASC) AS event_number
+FROM bloomreach_raw.campaign
+WHERE timestamp >= '2024-01-01'
